ac0919892862132ac068b8195fce162b4bc8f0f7
Kaisa Ets <kaisa.ets@voco.ee>
2024-10-10 14:14:40 +0300

    Read data from file

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const path = require('path')
+const fs = require('node:fs')
+
 const app = express()
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res)=>{
-    res.send('test nodemon')
+    fs.readFile('./tasks', 'utf8', (err,data) => {
+        if (err){
+            console.log(err)
+            return
+        } 
+        const tasks = data.split('\n')
+        res.render('index', {tasks: tasks})
+    } )
+    
 } )
 app.listen(3001, () => {
     console.log('Server is started http://localhost:3001')
